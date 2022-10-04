68d61772676143199b23538e4c2d7835ecffeea5
mfrodrigoenacom <rodrigo.fonseca@enacom.com.br>
2022-10-04 13:29:05 -0300

    feat: add users

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+  const user = [
+    { name: 'John Doe', email: 'john@example.com' },
+  ];
+  res.status(200).send(user);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).send(req.body);
 });
 
 module.exports = app;
