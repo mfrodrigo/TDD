6bbe43a6d78924316d30678c27014fb9fd7cde0f
mfrodrigoenacom <rodrigo.fonseca@enacom.com.br>
2022-10-06 10:50:23 -0300

    feat: add test false user

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,14 +1,32 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-const email = `${Date.now()}@gmail.com`;
+const email = `auth${Date.now()}@gmail.com`;
 
 test('Deve receber token ao logar', () => app.services.users
-  .save({ name: 'John', email, password: '123456' })
+  .save({ name: 'Auth', email, password: '123456' })
   .then(() => request(app)
     .post('/auth/signin')
     .send({ email, password: '123456' }))
   .then((res) => {
     expect(res.status).toBe(200);
     expect(res.body.token).not.toBeUndefined();
+  }));
+
+test('Não deve autenticar com senha errada', () => app.services.users
+  .save({ name: 'Auth2', email: `auth2${email}`, password: '123456' })
+  .then(() => request(app)
+    .post('/auth/signin')
+    .send({ email: `auth2${email}`, password: '12456' }))
+  .then((res) => {
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Usuário inválido.');
+  }));
+
+test('Não deve autenticar com senha errada', () => request(app)
+  .post('/auth/signin')
+  .send({ email: `auth3${email}`, password: '12456' })
+  .then((res) => {
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Usuário inválido.');
   }));
